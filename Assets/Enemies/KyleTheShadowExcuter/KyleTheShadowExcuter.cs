c04d0ac1ff052ef8466f152cb5f908cd8982665f
oty0365 <163986958+oty0365@users.noreply.github.com>
2024-11-26 23:34:47 +0900

    wip

--- Assets/Enemies/KyleTheShadowExcuter/KyleTheShadowExcuter.cs
+++ Assets/Enemies/KyleTheShadowExcuter/KyleTheShadowExcuter.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Enemies;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class KyleTheShadowExcuter : BossAi
 {
@@ -23,17 +24,45 @@ public class KyleTheShadowExcuter : BossAi
                 monsterAni.SetInteger("behave",1);
                 Chase();
                 break;
-                
+            case Behavior.Attack1:
+                monsterAni.SetInteger("behave",0);
+                monsterAni.SetBool("attacking",true);
+                monsterAni.SetTrigger("attack1");
+                break;
+            case Behavior.Attack2:
+                monsterAni.SetInteger("behave",0);
+                monsterAni.SetBool("attacking",true);
+                monsterAni.SetTrigger("attack2");
+                break;
         }
         EnemyFlip();
     }
 
     public override void Think()
     {
+        monsterAni.SetBool("attacking",false);
         var distance = Vector2.Distance(gameObject.transform.position, playerdata.transform.position);
         if (distance >=skills[0].skillMinRange && distance <= skills[0].skillMaxRange)
         {
-            behavior = Behavior.Chase;
+            var skills = Random.Range(0, 2);
+            switch (skills)
+            {
+                case 0:
+                    behavior = Behavior.Chase;
+                    break;
+                case 1:
+                    behavior = Behavior.Attack2;
+                    break;
+            }
+        }
+        else if (distance >=skills[1].skillMinRange && distance <= skills[1].skillMaxRange)
+        {
+            behavior = Behavior.Attack1;
+
+        }
+        else if (distance >=skills[2].skillMinRange && distance <= skills[2].skillMaxRange)
+        {
+            behavior = Behavior.Attack2;
         }
         else
         {
